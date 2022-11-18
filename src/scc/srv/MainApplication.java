ba503e72eaf49e3f4f7bd93f7f6191238950b8e2
Antonio <a.brejo@campus.fct.unl.pt>
2022-11-18 18:38:58 +0000

    Keys update

--- src/scc/srv/MainApplication.java
+++ src/scc/srv/MainApplication.java
@@ -16,12 +16,12 @@ public class MainApplication extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
 	private Set<Class<?>> resources = new HashSet<Class<?>>();
 
-	public static final String STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sccstwesteurope58152;AccountKey=R7HRsgWLnonRzrhdmaMgPsJiK2+IN66AO5OgWR341Fcj0hW/flx5pd6HSivuKgAwwCSNNk4fZ8pE+ASt+nsBQw==;EndpointSuffix=core.windows.net";
+	public static final String STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sccstwesteurope58152;AccountKey=lyrEMvZSTlOQ02P1RHd4nXw48pIbiQukRKKeOEP6Pb5H2hEuB2pQJTrLQqhyM7u/3dLZJNb4yM5/+AStAE7ODg==;EndpointSuffix=core.windows.net";
 	public static final String CONNECTION_URL = "https://scc2358152.documents.azure.com:443/";
-	public static final String DB_KEY = "Lz27NP1VmRREZJOOKAnYIbtD2AGbpwxpTnXwxnYyGRiEwxeUbjsAw4OOfDDkFSUpDGqdW0wYQc6bACDb0N4DTQ==";
+	public static final String DB_KEY = "pDGK9sE9PUXsg0nLhEcfu0YQLxgIiHJVbwLPnEdTxZqc9w7jLFAA34JVDW5cO3j50CjiZxtvtr1UACDbnIrVTg==";
 	public static final String DB_NAME = "scc23db58152";
 	public static final String REDIS_HOSTNAME = "rediswesteurope58152.redis.cache.windows.net";
-	public static final String REDIS_KEY = "jJm1HZsPQgqChx53LXKNZI2sAwTyu3xVgAzCaOkipTs=";
+	public static final String REDIS_KEY = "2WP061eo5XbykzjX9Vf7Zal5SdFDK95DYAzCaFqVyPI=";
 	public static final String SEARCH_PROP_FILE = "search-azurekeys-westeurope.props";
 	public static final String PROP_SERVICE_NAME = "scc23cs58152";
 	public static final String PROP_QUERY_KEY = "1HgHne4L03bVgPAxOURoyeGI31gxpbi6K6ap930T6ZAzSeDsed5Q";
